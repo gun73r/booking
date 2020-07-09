018fec5970ce66cf3f56fb33a79c4f1ca6c66c84
gun73r <alexander.kulakousky@gmail.com>
2020-07-09 20:07:30 +0300

    added full registration

--- booking/settings.py
+++ booking/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iss2yabjqmmwsz8uagbm_%0rmis&)9p5_w0x#!f4w@(sohtf_l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
