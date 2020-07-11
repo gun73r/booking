b1c6fc97ee2df464429f775bdcbb571dbde99fdb
Sergey Naseko <chalfee@mail.ru>
2020-07-11 14:45:20 +0300

    User model fix

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
 
 class Image(models.Model):
@@ -6,27 +7,17 @@ class Image(models.Model):
     pub_date = models.DateTimeField()
 
 
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=128)
-    full_name = models.CharField(max_length=300)
+class User(DjangoUser):
     birth_date = models.DateField()
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, unique=True)
     credit_card = models.CharField(max_length=4, blank=True)
     profile_image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
 
-    class Meta:
-        abstract = True
-
-
-class Customer(User):
-    pass
-
 
 class Comment(models.Model):
     content = models.TextField()
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     rate = models.PositiveSmallIntegerField()
     photos = models.ForeignKey(Image, on_delete=models.CASCADE)
@@ -47,8 +38,8 @@ class Apartments(models.Model):
     livers_amount = models.PositiveSmallIntegerField()
     description = models.TextField()
     apartments_type = models.CharField(max_length=50, choices=TYPES)
-    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    rate = models.FloatField()
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True, null=True)
+    rate = models.FloatField(blank=True, null=True)
     phone = models.CharField(max_length=20)
 
 
@@ -56,7 +47,7 @@ class Order(models.Model):
     settlement_date = models.DateField()
     eviction_date = models.DateField()
     apartments = models.ForeignKey(Apartments, on_delete=models.CASCADE)
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Hotel(models.Model):
@@ -74,7 +65,4 @@ class Hotel(models.Model):
     rate = models.FloatField(blank=True, null=True)
     description = models.TextField()
     phone = models.CharField(max_length=20)
-
-
-class Owner(User):
-    hotels = models.ForeignKey(Hotel, on_delete=models.CASCADE, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
